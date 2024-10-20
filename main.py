e72e3658d4661ff77e2345677e22850dd0120071
MatchPort <131888743+MatchPort@users.noreply.github.com>
2024-10-20 01:57:02 -0400

    Update main.py
    Fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pypresence import Presence
 import win32com.client
 import os
 
-# Discord client ID from the developer portal
+
 client_id = '1240303183989833779'
 RPC = Presence(client_id)
 RPC.connect()
@@ -20,12 +20,12 @@ def get_itunes_info():
             'position': itunes.PlayerPosition,
             'artwork': None
         }
-        # Try to get the artwork
+
         try:
             if current_track.Artwork.Count > 0:
                 artwork = current_track.Artwork.Item(1)
                 artwork_path = os.path.join(os.getcwd(), 'artwork.jpg')
-                # Ensure the artwork file can be written to
+
                 if os.path.exists(artwork_path):
                     os.remove(artwork_path)
                 artwork.SaveArtworkToFile(artwork_path)
@@ -39,16 +39,16 @@ def get_itunes_info():
 while True:
     song_info = get_itunes_info()
     if song_info:
-        # Use the asset key for the artwork
+
         large_image_key = 'artwork' if song_info['artwork'] else None
         
         RPC.update(
             state=f"by {song_info['artist']}",
             details=song_info['name'],
-            large_image=large_image_key,  # Use the artwork asset key
+            large_image=large_image_key,  
             large_text=song_info['album'],
             start=time.time() - song_info['position']
         )
     else:
         RPC.clear()
-    time.sleep(5)  # Update every 5 seconds
+    time.sleep(5)  
